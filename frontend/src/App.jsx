86d465c6855ae4fdbdcf178bfbb71e13ef43d30b
Daniel Liu <danielliu070809@gmail.com>
2026-04-24 23:34:12 -0500

    Wire BrowserRouter and AuthProvider into app entry point

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+// TODO: import all page components here
+
 function App() {
   return (
-    <div>
-      <h1>Cougar Planner</h1>
+    <div className="min-h-screen bg-bg-primary text-text-primary">
+      <Navbar />
+      <main className="pt-16">
+        <Routes>
+          {/* TODO: define all routes here */}
+        </Routes>
+      </main>
     </div>
   )
 }
