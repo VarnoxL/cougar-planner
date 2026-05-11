08218342ef797f08f740dad9857b8ef8a4919b82
Daniel Liu <danielliu070809@gmail.com>
2026-05-10 23:42:07 -0500

    Add Fall 2026 semester option and reorder by relevance

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,7 +2,8 @@ export const VALID_GRADES = ['A', 'B', 'C', 'D', 'F', 'W']
 export const DAY_ORDER = ['M', 'T', 'W', 'R', 'F']
 
 export const SEMESTER_OPTIONS = [
-    { value: '202580', label: 'Fall 2025' },
-    { value: '202620', label: 'Spring 2026' },
     { value: '202650', label: 'Summer 2026' },
+    { value: '202680', label: 'Fall 2026' },
+    { value: '202620', label: 'Spring 2026' },
+    { value: '202580', label: 'Fall 2025' },
   ]
